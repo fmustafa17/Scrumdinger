b494381dd22b6bddb48cdc613e5c726618f774de
Farhana <farhana.swe@gmail.com>
2022-10-10 17:16:09 -0700

    add custom label style

--- TrailingIconLabelStyle.swift
+++ TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Farhana Mustafa on 10/10/22.
 //
 
-import Foundation
+import SwiftUI
+
+/// Puts title first, then the icon. Labels are the opposite
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    // Because you defined the label style as a static property, you can call it using leading-dot syntax, which makes your code more readable.
+    static var trailingIcon: Self { Self() }
+}
