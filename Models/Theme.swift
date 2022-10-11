293ffc8f887767c1cf0cab41c82b01b6a061b0e5
Farhana <farhana.swe@gmail.com>
2022-10-10 18:24:38 -0700

    add list and fix colors

--- Models/Theme.swift
+++ Models/Theme.swift
@@ -8,31 +8,34 @@
 import SwiftUI
 
 enum Theme: String {
-    case bubblegum
-    case buttercup
     case indigo
-    case lavender
     case magenta
-    case navy
     case orange
-    case oxblood
-    case periwinkle
-    case poppy
     case purple
-    case seafoam
-    case sky
-    case tan
     case teal
     case yellow
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .orange, .teal, .yellow: return .black
+        case .indigo, .magenta, .purple: return .white
         }
     }
     
     var mainColor: Color {
-        Color(rawValue)
+        switch self {
+        case .orange:
+            return Color.orange
+        case .teal:
+            return Color.teal
+        case .yellow:
+            return Color.yellow
+        case .indigo:
+            return Color.indigo
+        case .magenta:
+            return Color.pink
+        case .purple:
+            return Color.purple
+        }
     }
 }
