462fc8c7921000e72c87a8f2c2e0a378825350ad
Farhana <farhana.swe@gmail.com>
2022-10-23 19:11:31 -0700

    add history

---
+++ Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Farhana Mustafa on 10/23/22.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
